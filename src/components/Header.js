23592364f5333619d2b28464f220b63a74aec48c
Odette Gleen <nkargleeny@gmail.com>
2022-11-05 19:08:16 +0100

    To do Project

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+
+// const Header = () => (
+//   <header className="header">
+//     <h1>Todo App</h1>
+//     <p>Add todo item and mark as completed or delete it when done.</p>
+//   </header>
+// );
+
+// export default Header;
---
+++ src/components/Todo.js
@@ -0,0 +1,27 @@
+// import React, { useState } from 'react';
+
+// const Todo = (todoList) => {
+//   const list = todoList.todoList;// eslint-disable-line
+//   const editTodo = (e) => {
+//     e.target.classList.add('hide');
+//   };
+//   const [edit, setEdit] = useState('');
+//   const changeTask = () => {// eslint-disable-line
+
+//   };
+//   return (
+//     <div>
+//       {
+//         list.map((item) => (// eslint-disable-line
+//           <div className="todo-div">
+//             <p className="task" onDoubleClick={editTodo}>{item.task}</p>
+//             <input type="text" className="inp" onChange={(e) => setEdit(e.target.value)} />
+//             <p>{edit}</p>
+//           </div>
+//         ))
+// }
+//     </div>
+//   );
+// };
+
+// export default Todo;
